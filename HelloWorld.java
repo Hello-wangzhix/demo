2d01a4472920412d218b9d5263421f052f7fb742
IUwangzx <=wangzhixian228@163.com>
2019-10-23 16:09:35 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class hello{
+    public static void main(String [] args){
+    	System.out.println("hello world");
+	System.out.println("china no.1");
+	System.out.println("i love china");
+	System.out.println("basketball piano");
+}
+}
